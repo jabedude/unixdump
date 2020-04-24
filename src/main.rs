ec8db4cb0b2bee9f6dc0c7fe7cf3a50129cafa7a
Joshua Abraham <sinisterpatrician@gmail.com>
2020-04-24 19:44:31 -0400

    Add usage statement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
+use log::*;
+
+fn usage() -> ! {
+    eprintln!("unixdump <path to unix socket>");
+    std::process::exit(1);
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        usage();
+    }
+
+    let target_pid = args[1].parse::<i32>().expect("Error getting PID argument");
+    info!("targ pid: {}", target_pid);
 }
