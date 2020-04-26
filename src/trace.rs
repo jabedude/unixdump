d05bb62479bd2ebeaaa880c63ad35ea9c68c2ce1
Joshua Abraham <sinisterpatrician@gmail.com>
2020-04-26 18:32:27 -0400

    WIP: small cleanups for trace code

--- src/trace.rs
+++ src/trace.rs
@@ -4,9 +4,7 @@ use nix::sys::ptrace;
 use crate::errors::*;
 
 pub (crate) fn trace_read(pid: Pid) -> Result<()> {
-    eprintln!("tracing : {:?}", pid);
-    ptrace::seize(pid, ptrace::Options::PTRACE_O_TRACESYSGOOD | ptrace::Options::PTRACE_O_TRACEEXEC | ptrace::Options::PTRACE_O_TRACEEXIT)?;
-    eprintln!("seized : {:?}", pid);
+    ptrace::seize(pid, ptrace::Options::PTRACE_O_TRACESYSGOOD)?;
     Ok(())
 }
 
@@ -15,40 +13,23 @@ mod tests {
     use super::*;
     use nix::unistd::{fork, ForkResult};
     use nix::unistd::execv;
-    use nix::sys::wait::waitpid;
-    use nix::sys::ptrace;
     use nix::sys::signal::{kill, Signal};
-    use std::thread;
-    use std::time::Duration;
     use std::ffi::CString;
 
-    use rusty_fork::rusty_fork_id;
-    use rusty_fork::rusty_fork_test;
-    use rusty_fork::rusty_fork_test_name;
-
-    //rusty_fork_test! {
     #[test]
     fn test_trace_read() {
         let path = "/tmp/.trace_test_sock";
 
         match fork() {
             Ok(ForkResult::Parent { child, .. }) => {
-                //thread::sleep(Duration::from_millis(9000));
-                //let res = waitpid(child, None).unwrap();
-                //eprintln!("res {:?}", res);
-
                 eprintln!("{:?}", trace_read(child));
                 kill(child, Some(Signal::SIGKILL)).unwrap();
-                std::fs::remove_file(&path);
+                let _ = std::fs::remove_file(&path);
             },
             Ok(ForkResult::Child) => {
-                //ptrace::traceme().unwrap();
-
-                execv(&CString::new("/bin/nc").unwrap(), &[&CString::new("/bin/nc").unwrap(), &CString::new("-U").unwrap(), &CString::new("/tmp/.trace_test_sock").unwrap(), &CString::new("-l").unwrap()]).unwrap();
-                eprintln!("ret");
+                execv(&CString::new("/bin/nc").unwrap(), &[&CString::new("/bin/nc").unwrap(), &CString::new("-U").unwrap(), &CString::new(path).unwrap(), &CString::new("-l").unwrap()]).unwrap();
             },
             Err(_) => panic!("Fork failed"),
         }
     }
-    //}
 }
